10076aaa6a77b8a128b15230723ddf013a738e5f
Mucahit Yildiz <alperenyildiz21@gmail.com>
2023-01-15 18:56:20 -0500

    Test git

--- src/models/leagueModel.js
+++ src/models/leagueModel.js
@@ -1,19 +1,19 @@
 class LeagueModel {
     constructor(obj) {
-        this.numTeams = obj.numTeams;
-        this.numPlayoffTeams = this.getNumPlayoffTeams();
+        this.teams = obj.teams;
+        this.numPlayoffTeams = this.getNumPlayoffTeams(len(this.teams));
     }
 
-    getNumPlayoffTeams = () => {
+    getNumPlayoffTeams = numTeams => {
         // this.numTeams can't be less than 2. 
         // I hope I remember to validate this in the frontend.
-        if (this.numTeams < 4) {
+        if (numTeams < 4) {
             return 2;
         }
-        else if (4 <= this.numTeams <= 9) {
+        else if (4 <= numTeams <= 9) {
             return 4;
         }
-        else if (10 <= this.numTeams <= 19) {
+        else if (10 <= numTeams <= 19) {
             return 8;
         }
         return 16;
